a026b1f69a5936ecde9f0ae674eb03eba4301459
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2024-02-05 21:28:51 +0500

    Update Styling

--- src/Components/weather.jsx
+++ src/Components/weather.jsx
@@ -38,30 +38,35 @@ export default function Weather() {
   };
 
   return (
-    <div>
-      <h1>Weather App</h1>
-      <input
-        type="text"
-        value={city}
-        onChange={handleInputChange}
-        placeholder="Enter city name"
-      />
-      <button onClick={fetchData}>Get Weather</button>
+    <div className="weather-app">
+      <h1>Weather</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={city}
+          onChange={handleInputChange}
+          placeholder="Enter city name"
+        />
+        <button onClick={fetchData}>Get</button>
+      </div>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {error && <p className="error-message">{error}</p>}
       {weatherData && (
-        <div>
+        <div className="weather-info">
           <h2>
             {weatherData.name}, {weatherData.sys.country}
           </h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Description: {weatherData.weather[0].description}</p>
-          <img
-            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-            alt="Weather icon"
-          />
-          <p>Humidity: {weatherData.main.humidity}%</p>
-          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+          <div className="weather-details">
+            <p>Temperature: {weatherData.main.temp}°C</p>
+            <p>Feel Like: {weatherData.main.feels_like}°C</p>
+            <p>Description: {weatherData.weather[0].description}</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+              alt="Weather icon"
+            />
+            <p>Humidity: {weatherData.main.humidity}%</p>
+            <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+          </div>
         </div>
       )}
     </div>
